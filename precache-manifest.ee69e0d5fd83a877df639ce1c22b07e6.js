10a61b5cdb1ee34074fcdfab281b07f2184e5698
krishnao1 <krishnaqw4141@gmail.com>
2020-02-14 01:29:38 -0800

    Updates

---
+++ precache-manifest.ee69e0d5fd83a877df639ce1c22b07e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccc931ecb9fea18c53d1a819f5fda90",
+    "url": "/react-course-2-expensiffyy-app/index.html"
+  },
+  {
+    "revision": "f108007c4b31eb5c8da5",
+    "url": "/react-course-2-expensiffyy-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3af460b7a5bb4e7cc611",
+    "url": "/react-course-2-expensiffyy-app/static/css/main.cf5788d3.chunk.css"
+  },
+  {
+    "revision": "f108007c4b31eb5c8da5",
+    "url": "/react-course-2-expensiffyy-app/static/js/2.b0c021de.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-course-2-expensiffyy-app/static/js/2.b0c021de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af460b7a5bb4e7cc611",
+    "url": "/react-course-2-expensiffyy-app/static/js/main.e1660305.chunk.js"
+  },
+  {
+    "revision": "41266c7a0f3b3ea2d62e",
+    "url": "/react-course-2-expensiffyy-app/static/js/runtime-main.2ddb6ea3.js"
+  }
+]);
